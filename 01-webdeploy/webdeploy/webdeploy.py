0b296844b4d91ee5c44f01d82dac605dbb03c923
Mark Jones <mark.jones3@dvla.gov.uk>
2020-10-26 12:06:23 +0000

    Set up Cloud Front CDN and SSL

--- 01-webdeploy/webdeploy/webdeploy.py
+++ 01-webdeploy/webdeploy/webdeploy.py
@@ -17,11 +17,18 @@ import click
 
 from bucket import BucketManager
 from domain import DomainManager
+from certificate import CertificateManager
+from cdn import DistributionManager
+
+import util
+
 
 # Global variables
 session = None
 bucket_manager = None
 domain_manager = None
+cert_manager = None
+dist_manager = None
 
 
 @click.group()
@@ -31,7 +38,7 @@ def cli(profile):
     """Webdeploy deploys websites to AWS."""
     
     #ref globals above, then when re-assigned other functions have access to them
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
 
     session_cfg = {}
     if profile:
@@ -43,7 +50,9 @@ def cli(profile):
     print(session)
     bucket_manager = BucketManager(session)
     domain_manager = DomainManager(session)
-    pass
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
+
 
 
 
@@ -94,8 +103,39 @@ def setup_domain(domain):
     
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
     domain_manager.create_s3_domain_record(zone, domain, endpoint)
-    print("Domain configure: http://{}".format(domain))
+    print("Domain configured: http://{}".format(domain))
 
+@cli.command('find-cert')
+@click.argument('domain')
+def find_cert(domain):
+    """Find a certificate for <DOMAIN>."""
+    print(cert_manager.find_matching_cert(domain))
+
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    """Set up CloudFront CDN for DOMAIN pointing to BUCKET."""
+    dist = dist_manager.find_matching_dist(domain)
+
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert:  # SSL is not optional at this time
+            print("Error: No matching cert found.")
+            return
+
+        dist = dist_manager.create_dist(domain, cert)
+        print("Waiting for distribution deployment...")
+        dist_manager.await_deploy(dist)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+
+    return
 
 if __name__ == '__main__':
     cli()
